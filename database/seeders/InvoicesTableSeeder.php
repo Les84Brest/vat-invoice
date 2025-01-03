3f8f2640e9c96f09fbff9ebec92a3bd8b07a07aa
Aliaksei Semianiuk <seman17061984@gmail.com>
2025-01-03 09:45:16 +0300

    vueTable: invoice seed clean up

--- database/seeders/InvoicesTableSeeder.php
+++ database/seeders/InvoicesTableSeeder.php
@@ -2,10 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Company;
 use App\Models\Invoice;
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 
