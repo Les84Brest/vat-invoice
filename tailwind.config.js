0a8667739ce2a2ffeb967df7b4f483b4c4f365dd
Aliaksei Semianiuk <seman17061984@gmail.com>
2025-04-30 00:01:11 +0300

    loginRegisterStyle: welcome page styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,19 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+
             },
+            colors: {
+                'dark-blue': '#1d3557',
+                'text-color': '#173038',
+                'light-green': '#f1faee',
+                'light-gray': '#ebf2fa',
+                'primary-color': '#409eff',
+                'button-hover-bg': '#79bbff',
+            },
+            backgroundImage: {
+                'welcome-bg': "url('/public/assets/images/welcome-bg.webp')",
+            }
         },
     },
     plugins: [],
