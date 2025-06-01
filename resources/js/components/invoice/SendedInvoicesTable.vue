a6f2025ffec9784f881941d61e66d8b8f9cd3833
Aliaksei Semianiuk <seman17061984@gmail.com>
2025-06-01 13:16:04 +0300

    refactTablesSum: add sum functionality to sendIncoicesTable component

--- resources/js/components/invoice/SendedInvoicesTable.vue
+++ resources/js/components/invoice/SendedInvoicesTable.vue
@@ -1,5 +1,6 @@
 <template>
-    <el-table :data="props.invoicesList" v-loading="isLoading" style="width: 100%" border empty-text="Нет данных">
+    <el-table :data="props.invoicesList" v-loading="isLoading" style="width: 100%" border empty-text="Нет данных"
+        show-summary :summary-method="tableSumFunction">
         <el-table-column type="index" :index="startingIndex" label="№ п/п" width="45" />
         <el-table-column prop="recipient_company.tax_id" label="УНП получателя" width="100" />
         <el-table-column prop="recipient_company.title" label="Наименование получателя" />
@@ -22,8 +23,21 @@
         <el-table-column prop="action_date" label="Дата совершения" width="100" />
         <el-table-column prop="creation_date" label="Дата выставления" width="100" />
         <el-table-column prop="author.full_name" label="Автор" width="120" />
-        <el-table-column prop="total_wo_vat" label="Итоговая сумма НДС, рублей" />
-        <el-table-column prop="total" label="Итоговая сумма с НДС, рублей" />
+        <el-table-column prop="total_wo_vat" label="Итоговая сумма без НДС, рублей">
+            <template #default="scope">
+                {{ formatCurrency(scope.row.total_wo_vat) }}
+            </template>
+        </el-table-column>
+        <el-table-column prop="total_vat" label="Cумма НДС, рублей">
+            <template #default="scope">
+                {{ formatCurrency(scope.row.total_vat) }}
+            </template>
+        </el-table-column>
+        <el-table-column prop="total" label="Итоговая сумма с НДС, рублей">
+            <template #default="scope">
+                {{ formatCurrency(scope.row.total) }}
+            </template>
+        </el-table-column>
 
         <el-table-column min-width="70" label="Действия">
             <template #default="scope">
@@ -33,18 +47,18 @@
     </el-table>
     <el-pagination background :page-size="10" layout="prev, pager, next" :total="invoiceStore.totalInvoices"
         @current-change="handlePageChange" hide-on-single-page />
-    <PasswordConfirmDialog @user-password-confirmed="handlePasswordConfirmed" />
 </template>
 <script setup lang="ts">
-import { Invoice } from '@/types/invoice';
-import { useRouter } from 'vue-router';
-import { ref, computed } from 'vue';
-import { MoreFilled, More, EditPen, View, Select, Failed } from '@element-plus/icons-vue';
+import useVatTableSummaries from '@/composables/useVatTableSummaries';
+import { NOT_SUMMARIZED_COLUMNS_SEND_INVOICES } from "@/constants/tables";
 import { useInvoiceStore } from '@/store/invoice';
-import InvoiceTableRowActions from '@components/invoice/InvoiceTableRowActions.vue';
-import InvoiceStatus from '@components/invoice/InvoiceStatus.vue';
-import InvoiceType from "@components/invoice/InvoiceType.vue";
+import { Invoice } from '@/types/invoice';
 import { InvoiceAlowedActions } from '@/types/invoiceTable';
+import { formatCurrency } from '@/utils/format';
+import InvoiceStatus from '@components/invoice/InvoiceStatus.vue';
+import InvoiceTableRowActions from '@components/invoice/InvoiceTableRowActions.vue';
+import InvoiceType from "@components/invoice/InvoiceType.vue";
+import { computed, ref } from 'vue';
 
 const props = defineProps<
     {
@@ -54,14 +68,11 @@ const props = defineProps<
     }
 >();
 
-const ALOWED_ACTIONS = [
-    InvoiceTableRowActions
-];
+const tableSumFunction = useVatTableSummaries(NOT_SUMMARIZED_COLUMNS_SEND_INVOICES, " ");
 
 const invoiceStore = useInvoiceStore();
-const router = useRouter();
-
 const isLoading = ref<boolean>(false);
+
 const startingIndex = computed(() => {
     return (invoiceStore.currentPage - 1) * 10 + 1;
 });
@@ -70,7 +81,5 @@ const startingIndex = computed(() => {
 function handlePageChange(page: number) {
     invoiceStore.fetchCurrentInvoices(page);
 }
-function handlePasswordConfirmed() {
-    console.log('%cpassword confirmed', 'padding: 5px; background: DarkKhaki; color: Yellow;');
-}
+
 </script>
---
+++ resources/js/constants/tables.ts
@@ -0,0 +1,10 @@
+export const NOT_SUMMARIZED_COLUMNS_SEND_INVOICES = [
+    "author.full_name",
+    "creation_date",
+    "action_date",
+    "type",
+    "status",
+    "number",
+    "recipient_company.title",
+    "recipient_company.tax_id",
+];
